2e28ac80235487e95f59c707df0c001f671e654e
rauldimon <rauldimon2@gmail.com>
2020-02-18 11:26:06 +0200

    validator

--- src/components/Main/Input/index.tsx
+++ src/components/Main/Input/index.tsx
@@ -8,47 +8,60 @@ interface State {
   valueOfInput: string
 }
 
-// class Input extends React.Component<any> {
-//   state = {
-//     valueOfInput: '',
-//   }
+const paterns = /[^a-zA-Zа-яА-ЯёЁ $]/;
 
-//   render() {
-//     return (
-//       <>
-//         <input className={style.TextInput} type="text" value={this.state.valueOfInput}
-//         onChange={e => this.setState({ valueOfInput: e.target.value.replace("D [^0-9]", '') })}>}
-//         </input>
-//       </>
-//     )
-//   }
-// }
-// export default Input
+class Input extends React.Component<{},State> {
+  state = {
+    valueOfInput: '',
+  }
 
-
-
-const Input: React.FC = () => {
-  const [valueOfInput, setValueofInput] = React.useState('')
-  return <>
-    <div className={style.BlockInput}>
-      <button className={style.ButtonIcon}>
-        <img alt="icon" src={IconSearch}></img>
-      </button>
-      <input className={style.TextInput} type='text' pattern='\D [^0-9]' 
-        value={valueOfInput}
-        onChange={e => {
-          e.target.value.replace("D [^0-9]", '') ? setValueofInput(e.target.value) : setValueofInput('')
-        }}
-        >
-      </input>
-      <button className={style.ButtonIcon}>
-        <img alt="icon" src={IconKeyboard}></img>
-      </button>
-      <button className={style.ButtonIcon}>
-        <img alt="icon" src={IconVoicesearch}></img>
-      </button>
-    </div>
-  </>
+  render() {
+    return (
+      <>
+        <div className={style.BlockInput}>
+          <button className={style.ButtonIcon}>
+            <img alt="icon" src={IconSearch}></img>
+          </button>
+          <input className={style.TextInput} type="text" value={this.state.valueOfInput}
+          onChange={e => this.setState({ valueOfInput: paterns.test(e.target.value) ? this.state.valueOfInput : e.target.value })}>
+          </input>
+              <button className={style.ButtonIcon}>
+              <img alt="icon" src={IconKeyboard}></img>
+            </button>
+            <button className={style.ButtonIcon}>
+              <img alt="icon" src={IconVoicesearch}></img>
+            </button>
+        </div>
+      </>
+    )
+  }
 }
-
 export default Input
+
+
+
+// const Input: React.FC = () => {
+//   const [valueOfInput, setValueofInput] = React.useState('')
+//   return <>
+//     <div className={style.BlockInput}>
+//       <button className={style.ButtonIcon}>
+//         <img alt="icon" src={IconSearch}></img>
+//       </button>
+//       <input className={style.TextInput} type='text' pattern='/[^a-zA-Zа-яА-ЯёЁ $]/' 
+//         value={valueOfInput}
+//         onChange={e => {
+//           e.target.value.replace("/[^a-zA-Zа-яА-ЯёЁ $]", '') ? setValueofInput(e.target.value) : setValueofInput('')
+//         }}
+//         >
+//       </input>
+//       <button className={style.ButtonIcon}>
+//         <img alt="icon" src={IconKeyboard}></img>
+//       </button>
+//       <button className={style.ButtonIcon}>
+//         <img alt="icon" src={IconVoicesearch}></img>
+//       </button>
+//     </div>
+//   </>
+// }
+
+//export default Input
